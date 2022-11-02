75c8d7916a87ca5e92b2bfe82776c642af132c66
Niharika <thammali.niharika@accenture.com>
2022-11-02 12:34:12 +0530

    displaystar.php

---
+++ displaystar.php
@@ -0,0 +1,15 @@
+<?php
+	for($i=0;$i<=5;$i++){
+		
+		$i = readline('Enter a number: ');
+		
+		
+		for($j=1;$j<=$i;$j++){
+		
+		
+		 echo "*";
+	}
+			
+			echo " ".PHP_EOL;
+		}
+?>
---
+++ number.php
@@ -0,0 +1,11 @@
+<?php
+$i = readline('Enter a number: ');
+	for($i=0;$i<=5;$i++){
+		
+		for($j=1;$j<=$i;$j++){
+		
+			echo "*";
+	}
+			echo " ".PHP_EOL;
+		}
+?>
