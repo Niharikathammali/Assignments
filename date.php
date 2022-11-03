8d8c25205e7bbdc34eb1e8c4f7270daa01a831f8
Niharika <thammali.niharika@accenture.com>
2022-11-03 18:17:56 +0530

    added Assignment1

--- date.php
+++ date.php
@@ -1,4 +1,13 @@
 <?php
-$Date = "31-10-2022";
-echo date('y-m-d', strtotime($Date. ' + 10 days'));
+$Date = readline('Enter a Date: ');
+		
+
+$newDate = date('d-m-Y', strtotime('+10 days'));
+  
+    echo "After 10 days date is", $newDate;
+//$Date = readline('After 10 days date is : ');
+$newDate = date('d-m-Y', strtotime('-1 year'));
+  
+    echo "Last year for the given date was", $newDate;
+
 ?>
--- displaystar.php
+++ displaystar.php
@@ -1,15 +1,17 @@
-<?php
-	for($i=0;$i<=5;$i++){
-		
-		$i = readline('Enter a number: ');
-		
-		
-		for($j=1;$j<=$i;$j++){
-		
-		
-		 echo "*";
-	}
-			
-			echo " ".PHP_EOL;
-		}
-?>
+<?php  
+echo "enter the vaule to print stars =  ";
+$x = trim(fgets(STDIN));
+
+
+if ($x <= "5"){
+for($i=0;$i<=$x;$i++){  
+for($j=1;$j<=$i;$j++){  
+echo "* ";  
+}  
+echo "\n";  
+}  
+}
+else {
+  echo "Please enter the value lessthan 5 ";
+}
+?> 
