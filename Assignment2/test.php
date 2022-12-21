0928c7513e35628a15ba48b3c1bf5803e9e852d7
Niharikathammali <116630300+Niharikathammali@users.noreply.github.com>
2022-12-21 18:10:54 +0530

    test.php

---
+++ Assignment2/test.php
@@ -0,0 +1,173 @@
+<?php
+  
+if($_POST) {
+    $first_name = "";
+    $last_name= "";
+    $phone= "";
+    $birthdate= "";
+    $email= "";
+    $message = "";
+    $email_body = "<div>";
+    $nameErr = "";
+    $emailErr = "";
+    $phoneErr = "";
+    $birthdateErr = "";
+
+}
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+   if (empty($_POST["first_name"]))
+     {$nameErr = "Name is required";}
+   else
+     {
+     $firstname = test_input($_POST["first_name"]);
+     // check if name only contains letters and whitespace
+     if (!preg_match("/^[a-zA-Z ]*$/",$first_name))
+       {
+       $nameErr = "Only letters and white space allowed";
+       }
+     }
+	  if (empty($_POST["last_name"]))
+     {$nameErr = "Name is required";}
+   else
+     {
+     $lastname = test_input($_POST["last_name"]);
+     // check if name only contains letters and whitespace
+     if (!preg_match("/^[a-zA-Z ]*$/",$last_name))
+       {
+       $nameErr = "Only letters and white space allowed";
+       }
+     }
+  
+   if (empty($_POST["email"]))
+     {$emailErr = "Email is required";}
+   else
+     {
+     $email = test_input($_POST["email"]);
+     // check if e-mail address syntax is valid
+     if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email))
+       {
+       $emailErr = "Invalid email format";
+       }
+     }
+	 
+	 if (empty($_POST["phone"]))
+     {
+		 $phoneErr = "Phone Number is required";}
+   else
+     {
+     $phone = test_input($_POST["phone"]);
+     // check if e-mail address syntax is valid
+     if (!preg_match("/^\d{9}$/",$phone))
+       {
+       $phoneErr = "Invalid Phone Number ";
+       }
+     }
+	 
+	if (empty($_POST["birthdate"]))
+     {$birthdateErr = "birthdate is required";}
+   else
+     {
+     $birthdate = test_input($_POST["birthdate"]);
+	 
+	 
+     }
+
+   if (empty($_POST["message"]))
+     {$message = "";}
+   else
+     {$message = test_input($_POST["message"]);}
+
+}
+
+function test_input($data)
+{
+     $data = trim($data);
+     $data = stripslashes($data);
+     $data = htmlspecialchars($data);
+     return $data;
+}
+
+      /* if(isset($_POST['first_name'])) {
+        $first_name = filter_var($_POST['first_name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>First Name:</b></label>&nbsp;<span>".$first_name."</span>
+                        </div>";
+    }
+    if(isset($_POST['last_name'])) {
+        $last_name = filter_var($_POST['last_name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Last Name:</b></label>&nbsp;<span>".$last_name."</span>
+                        </div>";
+    }
+ 
+    if(isset($_POST['phone'])) {
+        $phone = filter_var($_POST['phone'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Mobile:</b></label>&nbsp;<span>".$phone."</span>
+                        </div>";
+    }
+    if(isset($_POST['email'])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$email."</span>
+                        </div>";
+    }
+      
+   
+    }
+      
+    if(isset($_POST['message'])) {
+        $message = htmlspecialchars($_POST['message']);
+        $email_body .= "<div>
+                           <label><b>Visitor Message:</b></label>
+                           <div>".$message."</div>
+                        </div>";
+    }
+      
+      
+    function test_input($data)
+    {
+         $data = trim($data);
+         $data = stripslashes($data);
+         $data = htmlspecialchars($data);
+         return $data;
+    }*/
+    
+        
+        
+        echo "Hello" .$first_name.' '.$last_name;
+        echo "<br>";
+        echo "Birthdate:" .$birthdate;
+        //print_r(date('d-m',strtotime($birthdate)));
+        if(date('d-m',strtotime($birthdate))==date('d-m')){
+        
+               echo "<br>"; 
+               echo "Today is your birthday " .$birthdate.PHP_EOL;
+                echo"<br>";
+        
+                echo "Happy Birthday $first_name $last_name";
+        
+                echo "</br>";
+        
+              }
+        
+              else{
+        
+                echo " Wrong date of birth";
+        
+              }
+              //echo "</br>";
+             
+        /*echo $birthdate;*/
+    
+        echo "Email:" .$email;
+        echo "<br>";
+        echo "Phone:" .$phone;
+        echo "<br>";
+        echo "Message:" .$message;
+        echo "<br>";
+ 
+?>
+ <input type="submit" name="return" value="Return" onClick="javascript:history.go(-1)" \>
