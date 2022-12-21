bdea8c5a4c31790c6d22dd8ee146c141d935964b
Niharikathammali <116630300+Niharikathammali@users.noreply.github.com>
2022-12-21 17:40:50 +0530

    datetest.php

--- Assignment1/datetest.php
+++ Assignment1/datetest.php
@@ -1,5 +1,6 @@
 <?php
      $date = readline('Enter a Date: ');
+    if (preg_match("/^(20[0-9]{2})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$date)) {
     
      $newDate = date('y*m*d',strtotime ( '+10 days' , strtotime ($date) ));
           
@@ -9,5 +10,14 @@
      $newDate = date('y*m*d',strtotime ( '-1 year' , strtotime ($date) ));
          
         echo " \nLast year for the given date was   ", $newDate;
+         }
+
+ 
+
+else {
+
+
+    return false;
+}
 
 ?>
